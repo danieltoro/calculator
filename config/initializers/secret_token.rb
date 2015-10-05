09c24b201ed7743aaa86ea827e32a22a630a5c5d
danieltoro <d.toro@outlook.com>
2015-10-05 17:27:51 -0300

    calculadora

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Appgym::Application.config.secret_token = '0a4f3523a521648e24d5d0d4ec2d57ac139346d09e7ca8d14ee74d0a720d5d8063bde18fb0a62efb88395f011cbd35abe05a6d114998024dfe3ec090ba8f4cab'
